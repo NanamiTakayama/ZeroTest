1c87bbd494a3d1aa7c8ff889e3952711c1358a08
NanamiTakayama <ns.potable@gmail.com>
2017-03-02 20:38:19 +0900

    3rd

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import time
 import os
 
 while True:
+        print(os.environ['LANG'])
 	print("Hello IoT from isaax")
-        print( os.environ['isaaxTEST'] )
+        print(os.environ['isaaxTEST'])
 	time.sleep(20)
        
 
